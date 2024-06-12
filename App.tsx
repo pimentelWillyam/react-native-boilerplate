a96ecd4132c51df330a941c86183659aa9cfae67
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-06-12 06:43:43 -0300

    feat: adding app

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styles from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo ao React Native com TypeScript!</Text>
+    </View>
+  );
+};
+
+
+export default App

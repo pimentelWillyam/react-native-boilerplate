aee439c75c5e373e774af2bccca70e9a1507258d
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-06-12 06:44:03 -0300

    feat: adding styles

---
+++ styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 20,
+    color: 'blue',
+  },
+});
+
+export default styles

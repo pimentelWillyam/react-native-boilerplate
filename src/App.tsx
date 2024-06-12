9f6cc0507ee127ec9c3a08cdb7ef093943f091cd
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-06-12 10:27:05 -0300

    feat: adding app, routes and homescreen

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+
+const App = () => {
+  return (
+  )
+}
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Teste 2</Text>
+    </View>
+  );
+}

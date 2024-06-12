c12b1e32a2c8e5936f7ff35786575e9f62cfbdc6
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-06-12 08:57:16 -0300

    first commit

--- index.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native'
-import App from './App'
-
-// Registra o componente principal da aplicação
-AppRegistry.registerComponent('React Native Boilerplate', () => App);
